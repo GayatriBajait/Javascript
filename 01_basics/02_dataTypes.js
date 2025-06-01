bdf546de30b5323c99c9f1a5b0fc8912fbb59cbd
Gayatri Bajait <161224039+GayatriBajait@users.noreply.github.com>
2025-06-01 09:39:24 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all JS code as newer version
+
+//alert("hello") // We are using nodejs, not browser
+
+console.log(3 + 3);
+// Code readability that should be high
+
+console.log("Gayatri");
+
+
+let name = "Gayatri"
+let age = 21
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// biigint
+// string => ""
+// boolean => true/false
+// null => nstandalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof null)
+//null is object in javaScript0
