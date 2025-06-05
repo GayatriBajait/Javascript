dd9eecd490c2d47ce5ba00171981b0a652519955
Gayatri Bajait <161224039+GayatriBajait@users.noreply.github.com>
2025-06-05 06:52:09 +0000

    comparision

--- 01_basics/04_Comparision.js
+++ 01_basics/04_Comparision.js
@@ -16,3 +16,10 @@ console.log(null >= 0);
 Comparisons convert null to a number, treating it as 0.
 That's why (3) null >= 0 is true and (1) null > 0 is false. */
 
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// ===
+
+console.log("2" === 2);
