4c7f1aeec2314ccd592b2a854a90d32f827bed31
Gayatri Bajait <161224039+GayatriBajait@users.noreply.github.com>
2025-06-05 20:53:01 +0000

    array

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -10,6 +10,28 @@ console.log(myArr[2]);
 
 // myArr.push(6)
 // myArr.push(7)
-// myArr.pop()
-comsole.log(myArr);
+// myArr.pop()       // Remove last value
 
+// myArr.unshift(9)  // Insert in the front of all value
+// myArr.shift()
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(3));
+
+const newArr = myArr.join()
+
+console.log(myArr);
+console.log(typeof newArr);
+
+// slice, splice
+
+console.log("A ", myArr);
+
+const myn1 = myArr.slice(1, 3)
+
+console.log(myn1);
+console.log("B ", myArr);
+
+const myn2 = myArr.splice(1, 3)
+console.log("C ", myArr);
+console.log(myn2);
