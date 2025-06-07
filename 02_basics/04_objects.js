55b4bc24d545ed261994095c02097a4a5ecefa6a
Gayatri Bajait <161224039+GayatriBajait@users.noreply.github.com>
2025-06-07 08:22:36 +0000

    JSON

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -22,4 +22,79 @@ const regularUser = {
 
 console.log(regularUser.fullname.userfullname.firstname);
 
-const obj1 = 
+const obj1 = {1: "a", 2: "b"}       // Target
+const obj2 = {3: "c", 4: "d"}       // Sorce
+const obj4 = {5: "e", 6: "f"}
+
+// const obj3 = { obj1, obj2, obj4 }
+// const obj3 = Object.assign({}, obj1, obj2, obj4)
+
+const obj3 = {...obj1, ...obj2, ...obj4}
+console.log(obj3);
+
+
+// When value's come from database
+
+const users = [
+    {
+        id: 1,
+        email: "g@gmail.com"
+    },
+    {
+        id: 1,
+        email: "g@gmail.com"
+    },
+    {
+        id: 1,
+        email: "g@gmail.com"
+    },
+    {
+        id: 1,
+        email: "g@gmail.com"
+    },
+    {
+        id: 1,
+        email: "g@gmail.com"
+    }
+]
+
+users[1].email
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+
+console.log(tinderUser.hasOwnProperty("isLogged"));  // Use for checking the property is available or not (If it is there so O/P either it is false)
+
+// Destructure
+
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "Gayatri"
+}
+
+//  course.courseInstructor
+
+const {courseInstructor: instructor} = course
+
+// console.log(courseInstructor);
+console.log(instructor);
+
+// Object API O/P 
+
+// {
+//     "name": "gayatri",
+//     "coursename": "js in hindi",
+//     "price": "free"
+// }
+
+// Array API O/P
+
+[
+    {},
+    {},
+    {}
+]
