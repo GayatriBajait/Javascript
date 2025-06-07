eec7aa1ca2f333836693f1f04387eec9eb490e2b
Gayatri Bajait <161224039+GayatriBajait@users.noreply.github.com>
2025-06-07 09:54:20 +0000

    function

--- 03_basics/01_function.js
+++ 03_basics/01_function.js
@@ -45,3 +45,36 @@ function loginUserMessage(username){
 }
 
 console.log(loginUserMessage());
+
+
+function calculateCartPrice(...num1){
+    return num1
+}
+
+console.log(calculateCartPrice(200, 400, 500));
+
+function calculateCartPriceOne(val1, val2, ...num1){
+    return num1
+}
+
+console.log(calculateCartPriceOne(200, 400, 500, 2000));
+
+const user = {
+    username: "Gayatri",
+    price: 199
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is $
+        {anyobject.price}`);
+    
+}
+
+handleObject(user)
+
+handleObject({
+    username: "sam",
+    price: 399
+})
+
+const myNewArray = [200, 400, 100, 600]
