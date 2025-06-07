a4b812448990d6c3249df21e978212e09957b26b
Gayatri Bajait <161224039+GayatriBajait@users.noreply.github.com>
2025-06-07 21:12:33 +0000

    control flow

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -1,20 +1,55 @@
 // if
 
-const isUserLoggedIn = true
+// const isUserLoggedIn = true
 
-if (2=="2"){
-    console.log("executed");
-}
+// if (2=="2"){
+//     console.log("executed");
+// }
 
 // <, >, <=, >=, ==, !=, ===, !==
 
-const temperature = 41
+// const temperature = 41
 
-if ( temperature === 40 ){
-    console.log("less than 50");
+// if ( temperature === 40 ){
+//     console.log("less than 50");
+// }
+
+// else {
+// console.log("temperature is greater than 50");
+// }
+
+
+
+
+// const score = 200;
+// let power; // Declare it with let (or var) so you can assign it later
+
+// if (score > 100) {
+//     power = "fly";
+//     console.log(`User power inside block: ${power}`);
+// }
+
+// console.log(`User power outside block: ${power}`);
+
+
+
+const balance = 1000
+
+if (balance > 500) {
+    console.log("less than 500");
 }
-
+else if (balance < 750) {
+    console.log("less than 750");
+    
+}
+else if (balance < 900) {
+    console.log("less than 750");
+        
+}
 else {
-console.log("temperature is greater than 50");
+    console.log("less than 1200");
+    
 }
+
+
 
