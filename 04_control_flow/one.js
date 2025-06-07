1ea600bbd6460cbc6d23820d0b5cd5f27f15651b
Gayatri Bajait <161224039+GayatriBajait@users.noreply.github.com>
2025-06-07 18:12:45 +0000

    iife and control flow

---
+++ 04_control_flow/one.js
@@ -0,0 +1,20 @@
+// if
+
+const isUserLoggedIn = true
+
+if (2=="2"){
+    console.log("executed");
+}
+
+// <, >, <=, >=, ==, !=, ===, !==
+
+const temperature = 41
+
+if ( temperature === 40 ){
+    console.log("less than 50");
+}
+
+else {
+console.log("temperature is greater than 50");
+}
+
