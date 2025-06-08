b3bcbc716daf1d7a272db864241d029be608b47e
Gayatri Bajait <161224039+GayatriBajait@users.noreply.github.com>
2025-06-08 09:21:37 +0000

    switch, truthy, loops

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -33,23 +33,36 @@
 
 
 
-const balance = 1000
+// const balance = 1000
 
-if (balance > 500) {
-    console.log("less than 500");
-}
-else if (balance < 750) {
-    console.log("less than 750");
+// if (balance < 500) {
+//     console.log("less than 500");
+// }
+// else if (balance < 750) {
+//     console.log("less than 750");
     
-}
-else if (balance < 900) {
-    console.log("less than 750");
+// }
+// else if (balance < 900) {
+//     console.log("less than 750");
         
+// }
+// else {
+//     console.log("less than 1200");
+    
+// }
+
+// Logical Oprators
+
+const userLoggedIn = true
+const debitCard =  true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+if (userLoggedIn && debitCard && 2 == 2) {
+    console.log("Allow to by course");
 }
-else {
-    console.log("less than 1200");
+
+if (loggedInFromGoogle || loggedInFromEmail) {
+    console.log("User logged in");
     
 }
-
-
-
---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,25 @@
+const userEmail = "g@gmail.com"
+
+if (userEmail) {
+    console.log("Got user email");
+} else {
+    console.log("Don't have user email");
+    
+}
+
+//  falsy values
+
+// galse, 0, -0, BigInt On, "", null, undefined, NaN
+
+//  truthy values
+
+//  "0", "false", " ", [], {}, function(){}  // In string
+
+// if (userEmail.length === 0) {
+//     console.log("Array is empty");
+    
+// }
+
+const emptyObj = {}
+
+if (Object.keys(emptyObj).length === 0)
